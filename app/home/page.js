e1d6d64c4b0bfea285ae30a5ab6472f468ec1398
AndrewKzx <khorzx01@gmail.com>
2023-11-17 12:49:31 +0800

    initial commit

--- app/home/page.js
+++ app/home/page.js
@@ -1,14 +1,20 @@
 "use client"
 import React, { useEffect, useState } from 'react';
+import Nav from '../../../component/Nav'
 
 export default function Home() {
 
   return (
-    <div className='mt-20 mx-10'>
-      <p className='font-bold text-careDarkGreen text-2xl'>Cameras</p>
-      <p className='font-bold text-careLightGreen text-sm'>1 Active Camera</p>
-      <div className='pt-8'>
-        <img src="http://localhost:5000/video_feed" alt="Live Stream" className='rounded-xl' />
+    <div className=''>
+      <div className=' bg-gray-200'>
+        <Nav/>
+      </div>
+      <div className='mx-10 mt-10'>
+        <p className='font-bold text-careDarkGreen text-3xl'>Cameras</p>
+        <p className='font-bold text-careLightGreen text-sm'>1 Active Camera</p>
+        <div className='pt-8'>
+          <img src="http://localhost:5000/video_feed" alt="Live Stream" className='rounded-xl' />
+        </div>
       </div>
     </div>
   );
