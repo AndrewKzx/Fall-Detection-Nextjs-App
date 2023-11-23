7f1c5e454e05bedde754eaa6c48e2f936d67ee79
AndrewKzx <khorzx01@gmail.com>
2023-11-23 11:22:22 +0800

    bug fix 2

---
+++ component/Nav.js
@@ -0,0 +1,30 @@
+// components/Nav
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import "/app/globals.css"
+
+
+const Nav = () => {
+
+    return (
+        <div className='flex flex-between items-center justify-center pt-4 pb-4'>
+            <nav className='bg-gray-200 shadow-lg h-16'>
+                <div className='flex flex-row'>
+                    <div className='flex'>
+                        <Image src='/assets/images/CareCamLogo.png' width={80} height={100} alt="logo pic"></Image>
+                    </div>
+                    <div className='flex items-center text-3xl font-extrabold'>
+                        <p className='text-careDarkGreen'>Care</p> <p className='text-careLightGreen'>Cam</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
+
+
+
