b6bdfea84de5462ba9586c4aa2cd91f361d56b03
AndrewKzx <khorzx01@gmail.com>
2023-11-26 21:29:45 +0800

    call function test

--- app/home/page.js
+++ app/home/page.js
@@ -42,7 +42,7 @@ export default function Home() {
     <div>
       {/* Rest of the component */}
       <div className=' bg-gray-200'>
-        <Nav/>
+        <Nav />
       </div>
       <div className='mx-10 mt-10'>
         <p className='font-bold text-careDarkGreen text-3xl'>Cameras</p>
@@ -50,27 +50,30 @@ export default function Home() {
         <div className='pt-8'>
           <img src="http://localhost:5000/video_feed" alt="Live Stream" className='rounded-xl' />
           {fallDetected && (
-          <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
-            <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-              <div className="mt-3 text-center">
-                <div className="mx-auto flex items-center justify-center h-12 w-12 pb-10">
-                  <img src="/assets/images/emergency icon 1.png" alt="Care Cam Logo" className='w-36'></img>
-                </div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">Fall Detected</h3>
-                <div className="mt-2 px-7 py-3">
-                  <p className="text-sm text-gray-500">
-                    A fall has been detected. Please check the camera footage to verify.
-                  </p>
-                </div>
-                <div className="items-center px-4 py-3">
-                  <button id="ok-btn" className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-300" onClick={handleDismissAlert}>
-                    OK
-                  </button>
+            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
+              <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+                <div className="mt-3 text-center">
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 pb-10">
+                    <img src="/assets/images/emergency icon 1.png" alt="Care Cam Logo" className='w-36'></img>
+                  </div>
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">Fall Detected</h3>
+                  <div className="mt-2 px-7 py-3">
+                    <p className="text-sm text-gray-500">
+                      A fall has been detected. Please check the camera footage to verify.
+                    </p>
+                  </div>
+                  <div className="items-center px-4 py-3">
+                    <button id="ok-btn" className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-300" onClick={handleDismissAlert}>
+                      OK
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+          <a href="tel:999" className="emergency-call-button">
+            Emergency Call
+          </a>
         </div>
       </div>
     </div>
